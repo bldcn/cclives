79370e9253a0d4f9485df419a215091c176ef062
root <root@debug010000002015.local.alipay.net>
2020-04-05 03:08:27 +0800

    snap2htmlDailyUpdate

--- snap.sh
+++ snap.sh
@@ -5,7 +5,7 @@ sed -i "s#\\\\\\\root\\\\\\\那个男人的影视仓库##g"  /root/snap2html/Dio
 sed -i "s#1>index#1>所有内容均来自公开分享_收藏自用#g" /root/snap2html/DiogenesList-master/index.html
 cp /root/snap2html/DiogenesList-master/index.html /root/snapGit/
 cd /root/snapGit
-git add -A
-git commit -m "push"
-git push -u origin master
+git add .
+git commit -m 'snap2htmlDailyUpdate'
+git push  https://github.com/bldcn/cclives.git
 
