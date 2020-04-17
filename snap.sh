828ee3eaec1fa3b689d86fd0d431342aee7508de
root <root@debug010000002015.local.alipay.net>
2020-04-17 17:17:09 +0800

    snap2htmlDailyUpdate

--- snap.sh
+++ snap.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
+﻿#!/bin/bash
 #cd /root/snap2html/DiogenesList-master/
-#python /root/snap2html/DiogenesList-master/diogeneslist.py /root/testpan004 index
-sed -i "s#\\\\\\\root\\\\\\\那个男人的影视仓库##g"  /root/snap2html/DiogenesList-master/index.html
-sed -i "s#1>index#1>所有内容均来自公开分享_收藏自用#g" /root/snap2html/DiogenesList-master/index.html
+#python /root/snap2html/DiogenesList-master/diogeneslist.py /root/影视资源 index
 cp /root/snap2html/DiogenesList-master/index.html /root/snapGit/
 cd /root/snapGit
 git add .
